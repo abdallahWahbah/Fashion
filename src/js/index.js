213b383964bb086899c7f4d7560c445b4abf1b03
abdowahbah <abdowahbah123@yahoo.com>
2021-10-09 20:43:06 +0200

    footer & nav links(smooth scroll) & animations

--- src/js/index.js
+++ src/js/index.js
@@ -167,3 +167,117 @@ var swiper = new Swiper(".blog__swiper", {
         prevEl: ".swiper-button-prev",
     },
   });
+
+
+// Smooth scrool when you click on a navigation link
+document.querySelector(".navigation__list").addEventListener("click", e =>
+{
+    e.preventDefault();
+    if(!e.target.classList.contains("navigation__link")) return;
+    const id = e.target.getAttribute("href");
+
+    // remove color from all links, add the color to the clicked link
+    const links = document.querySelectorAll(".navigation__link");
+    links.forEach(link=>
+    {
+      link.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    // get the section and scroll to it
+    const sectionCoords = document.querySelector(`${id}`).getBoundingClientRect();
+    window.scrollTo(
+    {
+      left: sectionCoords.left + window.pageXOffset,
+      top: sectionCoords.top + window.pageYOffset -45,
+      behavior: 'smooth'
+    }
+  );
+});
+
+
+// colorize the navigation link when you reach a certain section
+const colorizeSection = (sectionID, sectionOffset = "20%") =>
+{
+  var waypoint = new Waypoint({
+    element: document.getElementById(`${sectionID}`),
+    handler: function() {
+      // remove the color from each link first
+      const links = document.querySelectorAll(".navigation__link");
+      links.forEach(link=>
+      {
+        link.classList.remove("active");
+      });
+
+      // colorize that link only
+      const linkItem = document.querySelector(`a[href = "#${sectionID}"]`);
+      linkItem.classList.add("active");
+    },
+    offset: `${sectionOffset}`
+  });
+};
+
+colorizeSection("header", "-10%");
+colorizeSection("section__products");
+colorizeSection("section__features__slider");
+colorizeSection("section__reviews");
+colorizeSection("section__contact");
+colorizeSection("section__blogs__slider");
+
+
+// Animations
+const animation = (el, animation, sectionOffset = "90%") =>
+{
+  var waypoint = new Waypoint({
+    // element: document.querySelector(`.${sectionID}`),
+    element: el,
+    handler: function() {
+      this.element.classList.add("animate__animated");
+      this.element.classList.add(`${animation}`);
+      console.log(this.element)
+    },
+    offset: `${sectionOffset}`
+  });
+};
+
+// header
+document.querySelectorAll(".js--fadeIn").forEach(el =>
+{ 
+  animation(el, "animate__fadeIn");
+});
+
+// Exclusive products links
+// Collection details
+// Reviews
+// Contact 3rd box
+// Contact map
+// Footer 2nd two items
+document.querySelectorAll(".js--fadeInRightBig").forEach(el =>
+{ 
+  animation(el, "animate__fadeInRightBig");
+});
+
+// Exclusive products products
+// Featured products
+// Blogs
+document.querySelectorAll(".js--fadeInUp").forEach(el =>
+{ 
+  animation(el, "animate__fadeInUp");
+});
+
+// Collection
+// Contact 1st box
+// Contact form
+// Footer 1st two items
+document.querySelectorAll(".js--fadeInLeftBig").forEach(el =>
+{ 
+  animation(el, "animate__fadeInLeftBig");
+});
+
+// Heading__primary--container
+// Footer social icons
+document.querySelectorAll(".js--bounce").forEach(el =>
+{ 
+  animation(el, "animate__bounceIn");
+});
+
